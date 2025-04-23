68a32d6a7e9ea3cb6aa89b72b4e65bb4da4e538b
Linkhant19 <lkk19@bu.edu>
2025-04-23 17:22:42 -0400

    started gatcha page template

---
+++ app/gatcha/page.tsx
@@ -0,0 +1,12 @@
+// Lin Khant Ko's work
+
+// app/gatcha/page.tsx
+import Gatcha from "@/components/Gatcha";
+
+export default function GatchaView() {
+    return (
+        <>
+            <Gatcha />
+        </>
+    );
+}
