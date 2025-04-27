62747b4577b87908ece281491310db3526a51736
Linkhant19 <lkk19@bu.edu>
2025-04-27 16:54:34 -0400

    added button and functionality in Gatcha.tsx

--- components/Gatcha.tsx
+++ components/Gatcha.tsx
@@ -1,20 +1,41 @@
 // Lin Khant Ko's work
 
 // components/Gatcha.tsx
+"use client";
+
 import { Button } from "@mui/material";
+import { useState } from "react";
+import getCard from "@/lib/getCard";
+import Card from "@/components/Card"; 
 
 export default function Gatcha() {
+    // in this component, I will implement
+    // - title
+    // - image (if there is one)
+    // - button
+    const [card, setCard] = useState<{
+        name: string;
+        rarity: string;
+        description: string;
+        img: string;
+    } | null>(null);
+
     return (
-        <div className="flex flex-col items-center">
-            <h1>GET A RANDOM CARD</h1>
-            <Button
-                    sx={{ width: "80px" }}
-                    variant="contained"
-                    type="submit"
-            >
-                    GATCHA!
-            </Button>
+        <div className="flex flex-col items-center space-y-4">
+            <h1 className="text-2xl font-bold">GET A RANDOM CARD</h1>
         
+            {card && <Card gatchaPull={card} />}
+        
+            <Button
+                sx={{ width: "120px" }}
+                variant="contained"
+                onClick={() => {
+                const randomCard = getCard();
+                setCard(randomCard);
+                }}
+            >
+                GATCHA!
+            </Button>
         </div>
     );
 }
