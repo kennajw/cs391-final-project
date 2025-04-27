7cccc53d720567358e8def6924435dce343afc34
Daniel <danieldavydov0@gmail.com>
2025-04-27 15:12:31 -0400

    fix Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center h-20 bg-[#ccd5ae] border border-dashed">
-      <h2 className="text-4xl font-semibold p-4 text-center">
+    <header className="flex justify-center items-center h-20 bg-[#22223B] border border-dashed">
+      <h2 className="text-4xl text-[#FAF9FF] font-semibold p-4 text-center">
         <Link href="/">CS391 Final Project</Link>
       </h2>
     </header>
