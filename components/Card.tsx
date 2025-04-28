10b708912ef6ead09d45c40acc316508db6e5971
kennajw <kennajw@bu.edu>
2025-04-27 20:26:50 -0400

    styling text

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,8 +4,8 @@ import { GatchaProps } from "@/types";
 export default function Card({gatchaPull}: {gatchaPull: GatchaProps}) {
     return (
         <div className="flex flex-col rounded-md bg-[#FAF9FF] text-[#2B2B53] p-10 justify-center w-[80%] text-center">
-            <h1 className="p-2">{gatchaPull.name}</h1>
-            <h2 className="p-2">{gatchaPull.rarity}</h2>
+            <h1 className="p-2 text-4xl">{gatchaPull.name}</h1>
+            <h2 className="p-2 text-2xl">{gatchaPull.rarity}</h2>
             <img src={gatchaPull.img} alt="INSERT ALT DESC LATER" className="rounded-md w-100 h-100 m-auto"/>
             <p className="p-2">{gatchaPull.description}</p>
         </div>
