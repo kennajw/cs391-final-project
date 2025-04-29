d39add250543942596cbcc6fdbd9c358e63e814b
kennajw <kennajw@bu.edu>
2025-04-28 22:24:04 -0400

    added comments as well as changing the font to be more readable

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,12 @@
 // Kenna Wylie's work
 import { GatchaProps } from "@/types";
 
+// component that will display the pulled alien character (which is passed in as gatchaPull)
 export default function Card({gatchaPull}: {gatchaPull: GatchaProps}) {
     return (
+        // div that creates the card itself (bg, color, display, etc.)
         <div className="flex flex-col rounded-md bg-[#FAF9FF] text-[#2B2B53] p-10 justify-center w-[80%] text-center">
+            {/* the following tags (h1, h2, img, p) display the information of the aliens */}
             <h1 className="p-2 text-4xl">{gatchaPull.name}</h1>
             <h2 className="p-2 text-2xl">{gatchaPull.rarity}</h2>
             <img src={gatchaPull.img} alt="INSERT ALT DESC LATER" className="rounded-md w-100 h-100 m-auto"/>
--- components/Main.tsx
+++ components/Main.tsx
@@ -5,7 +5,7 @@ export default function Main() {
     return (
         <div className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)]" style={{ backgroundColor: "#2B2B53", color: "#FAF9FF" }}>
              <div className="p-10">
-                <h1 className=" text-center text-5xl p-10">Welcome to 4L14N5: A Space Gatcha!</h1>
+                <h1 className=" text-center text-5xl p-10">Welcome to 4L13N5: A Space Gatcha!</h1>
                 <h2 className="text-center text-3xl p-6">How to Play:</h2>
                 <ul className="text-xl space-y-4 list-decimal list-inside text-center">
                     <li>Press the Play Button Below</li>
