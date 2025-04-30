18a266a67aa627698e4e3df9f7c19aa484a4cb27
kennajw <kennajw@bu.edu>
2025-04-30 19:51:33 -0400

    accidentally on main oops but only changed by component

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,12 +5,14 @@ import { GatchaProps } from "@/types";
 export default function Card({gatchaPull}: {gatchaPull: GatchaProps}) {
     return (
         // div that creates the card itself (bg, color, display, etc.)
-        <div className="flex flex-col rounded-md bg-[#FAF9FF] text-[#2B2B53] p-10 justify-center w-[80%] text-center">
+        <div className="flex flex-col rounded-md bg-[#FAF9FF] text-[#2B2B53] p-10 justify-center w-[80%] mh-[60%] text-center">
             {/* the following tags (h1, h2, img, p) display the information of the aliens */}
-            <h1 className="p-2 text-4xl">{gatchaPull.name}</h1>
-            <h2 className="p-2 text-2xl">{gatchaPull.rarity}</h2>
-            <img src={gatchaPull.img} alt="INSERT ALT DESC LATER" className="rounded-md w-100 h-100 m-auto"/>
-            <p className="p-2">{gatchaPull.description}</p>
+            <h1 className="p-1 text-[4vw]">{gatchaPull.name}</h1>
+            <h2 className="p-1 text-[2vh]">{gatchaPull.rarity}</h2>
+            <img src={gatchaPull.img} alt="INSERT ALT DESC LATER" className="rounded-md w-80 h-80 m-auto"/>
+            <div className="flex h-30">
+                <p className="p-1 text-[1.7vh] m-auto">{gatchaPull.description}</p>
+            </div>
         </div>
     );
 }
