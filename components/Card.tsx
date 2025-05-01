45ed28c22512ba4c4066ac6ca23cd240a4d57d26
kennajw <kennajw@bu.edu>
2025-04-30 20:42:06 -0400

    small change to h1 font size in card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,7 +7,7 @@ export default function Card({gatchaPull}: {gatchaPull: GatchaProps}) {
         // div that creates the card itself (bg, color, display, etc.)
         <div className="flex flex-col rounded-md bg-[#FAF9FF] text-[#2B2B53] p-10 justify-center w-[80%] mh-[60%] text-center">
             {/* the following tags (h1, h2, img, p) display the information of the aliens */}
-            <h1 className="p-1 text-[4vw]">{gatchaPull.name}</h1>
+            <h1 className="p-1 text-[4.5vh]">{gatchaPull.name}</h1>
             <h2 className="p-1 text-[2vh]">{gatchaPull.rarity}</h2>
             <img src={gatchaPull.img} alt="INSERT ALT DESC LATER" className="rounded-md w-80 h-80 m-auto"/>
             <div className="flex h-30">
